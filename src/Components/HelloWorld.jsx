a4eede853a896047cc482bce27b8cb37859bdb5f
davidlechuga <david.lechuga@lakmisystems.com.mx>
2019-12-25 22:30:17 -0600

    webpack-react-config and eslint.

---
+++ src/Components/HelloWorld.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+//sin llaves y sin return 
+const HelloWorld = () => (
+    <h1>Hola mundo</h1>
+); 
+
+export default HelloWorld
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './Components/HelloWorld';
+import  './assets/styles/App.scss'
+
+ReactDOM.render(< HelloWorld />, document.getElementById('app'));
